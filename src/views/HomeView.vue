9fed9c23ba9b52a2d0fab965776edb4a1f4270c2
Ben Collins <ben.collins@outlook.com>
2026-03-07 20:54:22 +1000

    Home page tweaks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,35 +32,42 @@ function begin(mode: 'novice' | 'advanced') {
         <div class="row justify-content-center">
             <div class="col-lg-7 text-center">
                 <h1 class="display-4 fw-bold mb-3">
-                    <i class="bi bi-controller" /> Shmup Stage 1 Quiz
+                    Shmup Stage 1 Quiz
                 </h1>
-                <p class="lead mb-4">
-                    Think you know your shmup stage&nbsp;1 themes? Listen to the music and guess which game
-                    it's from! A random set of <strong>20 tracks</strong> will be selected for you.
+                <p class="lead mb-5">
+                    Think you know your shmups? How many can you pick from their stage 1 theme? A random set of <strong>20 tracks</strong> will be selected for you.
                 </p>
                 <h2 class="h5 mb-3">
                     Choose your Difficulty
                 </h2>
-                <div class="d-flex flex-column align-items-center gap-3 mb-4">
+                <div class="d-flex flex-column align-items-center gap-3 mb-5">
                     <button
                         class="btn btn-success btn-lg w-100 py-3"
-                        style="max-width: 320px"
+                        style="max-width: 400px"
                         @click="begin('novice')"
                     >
-                        <div class="fw-bold fs-5">Novice</div>
-                        <div class="small opacity-75">Select from a list of 4 games</div>
+                        <div class="fw-bold fs-5">
+                            Novice
+                        </div>
+                        <div class="small opacity-75">
+                            Select from a list of 4 games
+                        </div>
                     </button>
                     <button
                         class="btn btn-danger btn-lg w-100 py-3"
-                        style="max-width: 320px"
+                        style="max-width: 400px"
                         @click="begin('advanced')"
                     >
-                        <div class="fw-bold fs-5">Advanced</div>
-                        <div class="small opacity-75">Select from the whole list of games</div>
+                        <div class="fw-bold fs-5">
+                            Advanced
+                        </div>
+                        <div class="small opacity-75">
+                            Select from the whole list of games
+                        </div>
                     </button>
                 </div>
                 <p class="text-muted small mb-2">
-                    There are currently {{ totalSongs }} songs from {{ totalShmups }} shmups loaded in
+                    There are currently {{ totalSongs }} songs from {{ totalShmups }} shmups loaded in this quiz
                 </p>
                 <div>
                     <RouterLink to="/song-list">
