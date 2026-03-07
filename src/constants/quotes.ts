1a88f18720535bfebcd85bb684efcee0b4259485
Ben Collins <BenDTU@users.noreply.github.com>
2026-03-07 20:58:22 +1000

    Update quotes.ts, add game origins

--- src/constants/quotes.ts
+++ src/constants/quotes.ts
@@ -1,12 +1,13 @@
 export const quotes: string[] = [
     'Get Ready!',
-    'For great justice!',
-    "This one's for the fighters, this one's for the soliders",
-    'Be attitudes for gains',
-    "Don't let the enemy freak you out!",
-    '3, 2, 1, let\'s go!',
-    'A huge battleship is approaching fast!',
-    'Do you remember how many breads you\'ve eaten in your life?',
-    'First stop, enemy outpost. Let\'s see what you got.',
-    'Power up!',
+    'For great justice!', // Zero Wing
+    "This one's for the fighters, this one's for the soliders", // Dariusburst
+    'Be attitudes for gains', //Radiant Silvergun
+    "Don't let the enemy freak you out!", // Space Invaders Extreme 2
+    '3, 2, 1, let\'s go!', // R-Type Leo
+    'A huge battleship is approaching fast!', // Darius
+    'Do you remember how many breads you\'ve eaten in your life?', // Touhou 6
+    'First stop, enemy outpost. Let\'s see what you got.',  // Donpachi
+    'Bonus! Bonus! Bonus!', // Blazing Star
+    'I have to go on. I can\'t quit now' // Aero Fighters 2
 ]
